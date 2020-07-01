833489b2d13eec8b7702ee3c95ac628c9b31e37b
jjuiddong <jjuiddong@hanmail.net>
2020-07-01 21:10:41 +0900

    add backend node.js server

---
+++ Src/practice/backend/src/main.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+import Koa from "koa";
+import Router from "koa-router";
+import mongoose from "mongoose";
+//import serve from 'koa-static';
+//import path from 'path';
+//import send from 'koa-send';
+//import api from "./api";
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+
+const app = new Koa();
+const router = new Router();
+
+//router.use("/api", api.routes());
+//app.use(bodyParser());
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
